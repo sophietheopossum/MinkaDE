e3fd2d0b81c9503efc6c93f5ffe1f48577f35bb2
Seirra <sophietheopossum@yandex.ru>
2026-07-11 19:03:08 +0100

    implement scaling fixes via xwayland-satellite instead
    a bit of a refactor, you could say

--- shojiwm-env.fish
+++ shojiwm-env.fish
@@ -8,4 +8,6 @@ set -gx SHOJI_CONFIG $HOME/Documents/src/MinkaDE/ShojiWM/packages/config/src/ind
 
 # Uncomment to run the submodule build of xwayland-satellite instead of the
 # distro package (fixes non-(0,0) layout origins; upstream 33c344f, > 0.8.1):
-# set -gx SHOJI_XWAYLAND_SATELLITE_PATH $HOME/Documents/src/MinkaDE/xwayland-satellite/target/release/xwayland-satellite
+set -gx SHOJI_XWAYLAND_SATELLITE_PATH $HOME/Documents/src/MinkaDE/xwayland-satellite/target/release/xwayland-satellite
+# activate mixed mode scaling
+set -gx XWLS_LOGICAL_GEOMETRY 1
